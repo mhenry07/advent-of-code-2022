803ec44425f0a98b0bd557ff641746c4fbde1645
Mike Henry <82587+mhenry07@users.noreply.github.com>
2022-12-06 01:03:26 -0800

    Solution for Day 1: Calorie Counting

---
+++ day-01-calorie-counting/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://adventofcode.com/2022/day/1
+
+var caloriesPerElf = new List<int>();
+var splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+var groups = Data.Input.Split("\r\n\r\n", splitOptions);
+foreach (var group in groups)
+{
+    var entries = group.Split("\r\n", splitOptions);
+    caloriesPerElf.Add(entries.Select(e => int.TryParse(e, out var calories) ? calories : 0).Sum());
+}
+
+var maxCalories = caloriesPerElf.Max();
+
+Console.WriteLine($"The most calories an elf is carrying: {maxCalories}");
