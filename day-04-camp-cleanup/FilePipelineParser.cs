ff3b1136d49aae18105fd4db219247f8adf15587
Mike Henry <82587+mhenry07@users.noreply.github.com>
2022-12-06 01:03:30 -0800

    Add alternative implementations for Day 4 and benchmark

--- day-04-camp-cleanup/FilePipelineParser.cs
+++ day-04-camp-cleanup/FilePipelineParser.cs
@@ -12,8 +12,85 @@ public static class FilePipelineParser
 {
     private const int CharBufferLength = 256; // should be <= 1 KB if using stackalloc
 
+    // implementing both halves of the pipe seems comparable to only implementing the reader via PipeReader.FromStream
+    public static async Task<IReadOnlyList<TItem>> ParseFileAsync<TItem>(
+        string filename, Encoding encoding, IFormatProvider? provider, CancellationToken cancellationToken = default)
+        where TItem : ISpanParsable<TItem>
+    {
+        using var stream = File.OpenRead(filename);
+
+        var pipe = new Pipe();
+        var writingTask = FillPipeAsync(stream, pipe.Writer, cancellationToken);
+        var readingTask = ReadPipeAsync<TItem>(pipe.Reader, encoding, provider, cancellationToken);
+
+        await Task.WhenAll(writingTask, readingTask).ConfigureAwait(false);
+
+        return await readingTask;
+    }
+
+    public static async Task FillPipeAsync(
+        Stream sourceStream, PipeWriter writer, CancellationToken cancellationToken)
+    {
+        const int minimumBufferSize = 512;
+
+        while (true)
+        {
+            var memory = writer.GetMemory(minimumBufferSize);
+            try
+            {
+                var bytesRead = await sourceStream.ReadAsync(memory, cancellationToken).ConfigureAwait(false);
+                if (bytesRead == 0)
+                    break;
+
+                writer.Advance(bytesRead);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FillPipeAsync failed: {ex}");
+                break;
+            }
+
+            var result = await writer.FlushAsync(cancellationToken);
+
+            if (result.IsCompleted)
+                break;
+        }
+
+        await writer.CompleteAsync();
+    }
+
+    public static async Task<IReadOnlyList<TItem>> ReadPipeAsync<TItem>(
+        PipeReader reader, Encoding encoding, IFormatProvider? provider, CancellationToken cancellationToken)
+        where TItem : ISpanParsable<TItem>
+    {
+        var items = new List<TItem>();
+        Memory<char> charBuffer = new char[CharBufferLength]; // allocating once outside loop rather than stackalloc within loop
+        while (true)
+        {
+            var readResult = await reader.ReadAsync(cancellationToken).ConfigureAwait(false);
+            var buffer = readResult.Buffer;
+
+            while (TryReadLine(ref buffer, out ReadOnlySequence<byte> line))
+            {
+                var item = Parse<TItem>(line, charBuffer, encoding, provider);
+                items.Add(item);
+            }
+
+            reader.AdvanceTo(buffer.Start, buffer.End);
+
+            if (readResult.IsCompleted)
+                break;
+        }
+
+        await reader.CompleteAsync().ConfigureAwait(false);
+
+        return items;
+    }
+
     // based on https://learn.microsoft.com/en-us/dotnet/standard/io/pipelines
     // and https://timiskhakov.github.io/posts/exploring-spans-and-pipelines
+    // TODO: I think file read should be a Pipeline "writer" (e.g. FillPipeAsync)
+    //       and the parser and/or consumer should be a Pipeline "reader"?
     public static async IAsyncEnumerable<TItem> ParseLinesAsync<TItem>(
         string filename, Encoding encoding, IFormatProvider? provider)
         where TItem : ISpanParsable<TItem>
@@ -57,7 +134,7 @@ public static class FilePipelineParser
         return true;
     }
 
-    // TODO: benchmark `Span<char> charBuffer = stackalloc char[MaxStackallocLength]` here vs. Memory<T> outside loop
+    // TODO: benchmark `Span<char> charBuffer = stackalloc char[line.Length]` here vs. Memory<T> outside loop
     private static TItem Parse<TItem>(
         ReadOnlySequence<byte> line, Memory<char> charMemory, Encoding encoding, IFormatProvider? provider)
         where TItem : ISpanParsable<TItem>
--- day-04-camp-cleanup/Program.cs
+++ day-04-camp-cleanup/Program.cs
@@ -1,30 +1,217 @@
 ﻿// Day 4: Camp Cleanup
 // https://adventofcode.com/2022/day/4
 
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-var fullyContainedCount = 0;
-var overlapCount = 0;
-var assignmentPairsAsyncEnumerable =
-    FilePipelineParser.ParseLinesAsync<AssignmentPair>("input.txt", Encoding.UTF8, CultureInfo.InvariantCulture);
-await foreach (var assignmentPair in assignmentPairsAsyncEnumerable)
+internal class Program
 {
-    if (assignmentPair.HasFullyContainedRange())
-        fullyContainedCount++;
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Solution>();
+    }
 
-    if (assignmentPair.HasOverlap())
-        overlapCount++;
+    // private static async Task Main(string[] args)
+    // {
+    //     var sw = Stopwatch.StartNew();
+
+    //     var solution = new Solution();
+    //     var resultTasks = new[]
+    //     {
+    //         solution.GetResultsPipeReaderAsyncEnumerableAsync(),
+    //         solution.GetResultsPipeWriterReaderAsync(),
+    //         solution.GetResultsReadAllBytesSpanAsync(),
+    //         solution.GetResultsReadAllLinesAsync(),
+    //         solution.GetResultsReadLinesAsync(),
+    //         solution.GetResultsStreamReaderAsync()
+    //     };
+
+    //     await Task.WhenAll(resultTasks);
+
+    //     sw.Stop();
+
+    //     var result = await resultTasks[0];
+    //     for (int i = 1; i < resultTasks.Length; i++)
+    //     {
+    //         var resultN = await resultTasks[i];
+    //         if (resultN != result)
+    //             Console.WriteLine($"Mismatch: {resultN} doesn't match {result}");
+    //     }
+
+    //     // part 1
+    //     Console.WriteLine($"Assignment pairs where one range fully contains the other: {result.FullyContainedCount}");
+
+    //     // part 2
+    //     Console.WriteLine($"Assignment pairs with overlapping ranges: {result.OverlapCount}");
+
+    //     Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds:N2} ms for {result.TotalCount:N0} records");
+    // }
 }
 
-// part 1
-Console.WriteLine($"Assignment pairs where one range fully contains the other: {fullyContainedCount}");
+[InProcessAttribute]
+[MemoryDiagnoser]
+public class Solution
+{
+    [Benchmark]
+    public async Task<Results> GetResultsPipeWriterReaderAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        var assignmentPairsTask =
+            FilePipelineParser.ParseFileAsync<AssignmentPair>("input.txt", Encoding.UTF8, CultureInfo.InvariantCulture);
+        foreach (var assignmentPair in await assignmentPairsTask)
+        {
+            totalCount++;
 
-// part 2
-Console.WriteLine($"Assignment pairs with overlapping ranges: {overlapCount}");
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+    }
+
+    [Benchmark]
+    public async Task<Results> GetResultsPipeReaderAsyncEnumerableAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        var assignmentPairsAsyncEnumerable =
+            FilePipelineParser.ParseLinesAsync<AssignmentPair>("input.txt", Encoding.UTF8, CultureInfo.InvariantCulture);
+        await foreach (var assignmentPair in assignmentPairsAsyncEnumerable)
+        {
+            totalCount++;
+
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+    }
+
+    [Benchmark]
+    public async Task<Results> GetResultsReadAllBytesSpanAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        Memory<char> charMemory = new char[256];
+        Memory<byte> memory = await File.ReadAllBytesAsync("input.txt");
+        int start = 0, end = 0;
+        while (start < memory.Length)
+        {
+            var index = memory.Span.Slice(start).IndexOf((byte)'\n');
+            end = index >= 0 ? start + index : memory.Length - 1;
+
+            totalCount++;
+
+            var assignmentPair = Parse(memory.Slice(start, end - start + 1).Span, charMemory);
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+            
+            start = end + 1;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+
+        AssignmentPair Parse(Span<byte> line, Memory<char> charMemory)
+        {
+            var charBuffer = charMemory.Span;
+            var length = Encoding.UTF8.GetChars(line, charBuffer);
+            var chars = charBuffer.Slice(0, length).Trim();
+            return AssignmentPair.Parse(chars, CultureInfo.InvariantCulture);
+        }
+    }
+
+    [Benchmark]
+    public async Task<Results> GetResultsReadAllLinesAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        var allLines = await File.ReadAllLinesAsync("input.txt", Encoding.UTF8);
+        foreach (var line in allLines)
+        {
+            totalCount++;
+
+            var assignmentPair = AssignmentPair.ParseAllocating(line);
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+    }
+
+    [Benchmark]
+    public async Task<Results> GetResultsReadLinesAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        var linesAsyncEnumerable = File.ReadLinesAsync("input.txt", Encoding.UTF8);
+        await foreach (var line in linesAsyncEnumerable)
+        {
+            totalCount++;
+
+            var assignmentPair = AssignmentPair.ParseAllocating(line);
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+    }
+
+    [Benchmark]
+    public async Task<Results> GetResultsStreamReaderAsync()
+    {
+        var totalCount = 0;
+        var fullyContainedCount = 0;
+        var overlapCount = 0;
+        using var streamReader = new StreamReader("input.txt", Encoding.UTF8);
+        while (true)
+        {
+            var line = await streamReader.ReadLineAsync();
+            if (line == null)
+                break;
+
+            totalCount++;
+
+            var assignmentPair = AssignmentPair.ParseAllocating(line);
+            if (assignmentPair.HasFullyContainedRange())
+                fullyContainedCount++;
+
+            if (assignmentPair.HasOverlap())
+                overlapCount++;
+        }
+
+        return new Results(totalCount, fullyContainedCount, overlapCount);
+    }
+}
+
+public record Results(int TotalCount, int FullyContainedCount, int OverlapCount);
 
 // implementation
+// would it be worthwhile for this to be a record struct?
 public record AssignmentPair : ISpanParsable<AssignmentPair>
 {
     public Range Elf1 { get; init; }
@@ -41,7 +228,7 @@ public record AssignmentPair : ISpanParsable<AssignmentPair>
         if (TryParse(s, provider, out var result))
             return result;
 
-        throw new FormatException();
+        throw new FormatException($"{s}");
     }
 
     public static AssignmentPair Parse(string s, IFormatProvider? provider) =>
@@ -92,6 +279,24 @@ public record AssignmentPair : ISpanParsable<AssignmentPair>
 
         result = default;
         return false;
+    }
+
+    public static AssignmentPair ParseAllocating(string line)
+    {
+        var pair = line.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        return new AssignmentPair
+        {
+            Elf1 = ParseRangeAllocating(pair[0]),
+            Elf2 = ParseRangeAllocating(pair[1])
+        };
+    }
+
+    private static Range ParseRangeAllocating(string input)
+    {
+        var tokens = input.Split('-', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var start = int.Parse(tokens[0]);
+        var end = int.Parse(tokens[1]);
+        return new Range(start, end);
     }
 }
 
