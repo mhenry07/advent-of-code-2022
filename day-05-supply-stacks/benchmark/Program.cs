4603f32fcf579b94d46fef4e19204e73a44c56de
Mike Henry <82587+mhenry07@users.noreply.github.com>
2022-12-10 18:22:37 -0800

    Add benchmarks for day 5

---
+++ day-05-supply-stacks/benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Day05>();
+    }
+}
+
+[InProcessAttribute]
+[MemoryDiagnoser]
+public class Day05
+{
+    [Benchmark]
+    public Task<Results> GetResultsCharBuffersAsync() =>
+        Solution.GetResultsCharBuffersAsync();
+
+    [Benchmark]
+    public Task<Results> GetResultsStackRowsAsync() =>
+        Solution.GetResultsStackRowsAsync();
+}
