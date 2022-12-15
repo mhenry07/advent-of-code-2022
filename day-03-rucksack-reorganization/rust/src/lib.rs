189fba1bacb97410a2584368356010daafc050a8
Mike Henry <82587+mhenry07@users.noreply.github.com>
2022-12-15 01:26:49 -0800

    Reduce string copies by using string slices with lifetimes
    - this reduced "day 3/in memory" benchmark from ~162 ms to ~121 ms (25%)
    - "day 3/input.txt" barely changed from ~260 ms to ~250 ms

--- day-03-rucksack-reorganization/rust/src/lib.rs
+++ day-03-rucksack-reorganization/rust/src/lib.rs
@@ -7,13 +7,14 @@ use std::path::Path;
 pub fn run<P>(filename: P) -> Result<Results, Box<dyn Error>>
 where P: AsRef<Path> {
     let mut results = Results::new();
-    let mut group = Group::new();
     let mut i = 0;
+    let mut group_lines = [String::from(""), String::from(""), String::from("")];
     let lines = read_lines(filename)?;
     for line in lines {
         let num = i % 3;
-        group.add_sack(num, line?);
+        group_lines[num] = line?;
         if num == 2 {
+            let group = Group::new(group_lines[0].as_str(), group_lines[1].as_str(), group_lines[2].as_str());
             results.handle_group(&group)?;
         }
         i += 1;
@@ -25,12 +26,13 @@ where P: AsRef<Path> {
 /// Solves Day 3 from a string slice `input`
 pub fn run_lines(input: &str) -> Result<Results, Box<dyn Error>> {
     let mut results = Results::new();
-    let mut group = Group::new();
     let mut i = 0;
+    let mut group_lines = [""; 3];
     for line in input.lines() {
         let num = i % 3;
-        group.add_sack(num, line.to_string()); // TODO: find a way to eliminate to_string() if possible
+        group_lines[num] = line;
         if num == 2 {
+            let group = Group::new(group_lines[0], group_lines[1], group_lines[2]);
             results.handle_group(&group)?;
         }
         i += 1;
@@ -53,47 +55,32 @@ impl Results {
 
     fn handle_group(&mut self, group: &Group) -> Result<(), Box<dyn Error>> {
         self.sum_1 += group.compartment_priorities_sum()
-            .ok_or_else(|| "No badge found for group")?;
+            .ok_or_else(|| "Common compartment item not found for one or more rucksacks in group")?;
 
         self.sum_2 += group.badge_priority()
-            .ok_or_else(|| "No badge found for group")?;
+            .ok_or_else(|| "Badge not found for group")?;
 
         Ok(())
     }
 }
 
-struct Group {
-    sack_1: Option<String>,
-    sack_2: Option<String>,
-    sack_3: Option<String>
+struct Group<'a> {
+    sack_1: &'a str,
+    sack_2: &'a str,
+    sack_3: &'a str
 }
 
-impl Group {
-    fn new() -> Group {
-        Group { sack_1: None, sack_2: None, sack_3: None }
+impl<'a> Group<'a> {
+    fn new(sack_1: &'a str, sack_2: &'a str, sack_3: &'a str) -> Group<'a> {
+        Group::<'a> { sack_1, sack_2, sack_3 }
     }
 
-    fn add_sack(&mut self, num: i32, sack: String) {
-        if num == 0 {
-            self.sack_1 = Some(sack);
-        } else if num == 1 {
-            self.sack_2 = Some(sack);
-        } else if num == 2 {
-            self.sack_3 = Some(sack);
-        }
-    }
-
-    fn get_sack(&self, num: i32) -> Option<&str> {
-        let sack = match num {
-            0 => &self.sack_1,
-            1 => &self.sack_2,
-            2 => &self.sack_3,
-            _ => &None
-        };
-
-        return match sack {
-            Some(s) => Some(s.as_str()),
-            None => None
+    fn get_sack(&self, num: usize) -> &str {
+        match num {
+            0 => self.sack_1,
+            1 => self.sack_2,
+            2 => self.sack_3,
+            _ => panic!("Invalid num: {num}")
         }
     }
 
@@ -106,8 +93,8 @@ impl Group {
         Some(sum)
     }
 
-    fn both_compartments_priority(&self, num: i32) -> Option<i32> {
-        let sack = self.get_sack(num)?;
+    fn both_compartments_priority(&self, num: usize) -> Option<i32> {
+        let sack = self.get_sack(num);
         let (left, right) = sack.split_at(sack.len() / 2);
         let common_item = find_common_compartment_item(left, right)?;
 
@@ -122,12 +109,13 @@ impl Group {
     }
 
     fn find_badge(&self) -> Option<char> {
-        for i1 in self.get_sack(0)?.chars() {
-            for i2 in self.get_sack(1)?.chars() {
+        for i1 in self.sack_1.chars() {
+            for i2 in self.sack_2.chars() {
                 if i2 != i1 {
                     continue;
                 }
-                for i3 in self.get_sack(2)?.chars() {
+
+                for i3 in self.sack_3.chars() {
                     if i3 == i2 && i2 == i1 {
                         return Some(i1)
                     }
